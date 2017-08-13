f8cc945725b340775acaf11cc8d5351bbb58eacf
Leonid Sislo <leonid.sislo@gmail.com>
2017-08-13 02:02:38 +0100

    Clean up directory structure.

--- simple-service/src/main/java/com/revolut/AccountOperations.java
+++
@@ -1,8 +0,0 @@
-package com.revolut;
-
-class AccountOperations {
-    /** Adds <code>money</code> to the account. */
-    static Account updateAccountWithMoney(Account account, double deltaMoney) {
-        return new Account(account.getId(), account.getOwnerId(), account.getMoney() + deltaMoney);
-    }
-}
